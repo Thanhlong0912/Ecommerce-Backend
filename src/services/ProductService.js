913ba83783f6b3b519ab82b417332380eba0f9fc
Thanhlong0912 <dinhthanhlong47@gmail.com>
2023-03-08 19:00:07 +0700

    update-product

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,8 +2,16 @@ const Product = require("../models/ProductModel");
 
 const createProduct = (newProduct) => {
   return new Promise(async (resolve, reject) => {
-    const { name, image, type, countInStock, price, rating, description } =
-      newProduct;
+    const {
+      name,
+      image,
+      type,
+      countInStock,
+      price,
+      rating,
+      description,
+      discount,
+    } = newProduct;
     try {
       const checkProduct = await Product.findOne({
         name: name,
@@ -22,6 +30,7 @@ const createProduct = (newProduct) => {
         price,
         rating,
         description,
+        discount,
       });
       if (newProduct) {
         resolve({
@@ -129,6 +138,7 @@ const getAllProduct = (limit, page, sort, filter) => {
   return new Promise(async (resolve, reject) => {
     try {
       const totalProduct = await Product.count();
+      let allProduct = [];
       if (filter) {
         const label = filter[0];
         const allObjectFilter = await Product.find({
@@ -161,9 +171,13 @@ const getAllProduct = (limit, page, sort, filter) => {
           totalPage: Math.ceil(totalProduct / limit),
         });
       }
-      const allProduct = await Product.find()
-        .limit(limit)
-        .skip(page * limit);
+      if (!limit) {
+        allProduct = await Product.find();
+      } else {
+        allProduct = await Product.find()
+          .limit(limit)
+          .skip(page * limit);
+      }
       resolve({
         status: "OK",
         message: "Success",
